edf1b87585af41ca40a70e6c16777c02c083e025
Saeed MJ <treegp@gmail.com>
2022-07-12 23:20:38 +0430

    Rebuild GenericRepository Class

--- ConnectToDB/GenericRepository.cs
+++ ConnectToDB/GenericRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 
 namespace ConnectToDB
 {
@@ -252,5 +253,45 @@ namespace ConnectToDB
 
 
 
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class Table : Attribute
+    {
+        public string Schema { get; set; }
+        public string Name { get; set; }
+
+
+        public Table(string schema = "dbo")
+        {
+            Schema = schema;
+            Name = typeof(Table).Name;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class Column : Attribute
+    {
+        public bool PrimaryKey { get; set; }
+        public bool Computed { get; set; }
+        public bool Required { get; set; }
+
+        public Column(bool required = false, bool computed = false, bool primaryKey = false)
+        {
+            PrimaryKey = primaryKey;
+            Computed = computed;
+            Required = required;
+        }
+    }
+
+    public class ColumnSpecifics
+
+    {
+        public PropertyInfo ColumnType { get; set; }
+        public string ColumnName { get; set; }
+        public string PropertyName { get; set; }
+        public bool Required { get; set; }
+        public bool PrimaryKey { get; set; }
+        public bool Computed { get; set; }
     }
 }
